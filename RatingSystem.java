bff6c681ba8bdc43a88e0d2903cfb03953667a43
Ricky Mosqueda-Torres <Ricardomosqueda123213@gmail.com>
2025-11-13 04:32:42 -0600

    Implemented RatingSystem: rateUser, rateEmployee, and calculateAverageRating

--- RatingSystem.java
+++ RatingSystem.java
@@ -1,7 +1,27 @@
+import java.util.List;
+
 public class RatingSystem {
 
-    // Method definitions (to be implemented)
-    public void rateUser(UserAccount user, double rating) {}
-    public void rateEmployee(Employee employee, double rating) {}
-    public void calculateAverageRating() {}
+    public void rateUser(UserAccount user, double rating) {
+        user.setRating(rating);
+        System.out.println("Rated user " + user.getName() + " with rating: " + rating);
+    }
+
+    public void rateEmployee(Employee employee, double rating) {
+        employee.setRating(rating);
+        System.out.println("Rated employee " + employee.getName() + " with rating: " + rating);
+    }
+
+    public double calculateAverageRating(List<Double> ratings) {
+        if (ratings == null || ratings.isEmpty()) {
+            return 0.0;
+        }
+
+        double sum = 0;
+        for (double r : ratings) {
+            sum += r;
+        }
+
+        return sum / ratings.size();
+    }
 }
